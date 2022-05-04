48d83c8674c547ae9a3c457ae616a558649da707
Kalidou <kalidou1309@gmail.com>
2022-05-04 05:23:50 +0000

    server side rendering

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export default {
     // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-    ssr: false,
+    ssr: true,
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
