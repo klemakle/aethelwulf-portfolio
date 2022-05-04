c029716a9e586c9fb6ba1260b31bbbbbf908e0ad
Kalidou <kalidou1309@gmail.com>
2022-05-04 04:59:27 +0000

    axios base url with node_env

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,7 +82,8 @@ export default {
     ],
 
     axios: {
-        baseURL: "https://aethelwulf13.netlify.app/",
+        baseURL: process.env.NODE_ENV === 'dev' ?
+            'http://localhost:3000' : 'https://aethelwulf13.netlify.app/',
     },
 
     bootstrapVue: {
