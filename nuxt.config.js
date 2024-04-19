bc59364c634a5b40b8e12a8604be04f6c53828f2
Kalidou DIA <49349007+klemakle@users.noreply.github.com>
2024-04-19 09:50:19 +0200

    Update nuxt.config.js
    add generate routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
     ssr: false,
     target: 'static',
     generate: {
-        crawler: false
+        routes: ['/blog/audit-pentest', '/blog/*']
     },
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
